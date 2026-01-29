65b97338e16e8a683978382c3b21466e6b1ea5ff
linkcleaning <han0829@gmail.com>
2026-01-29 22:46:12 +0900

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,9 +4,6 @@ import Hero from './components/Hero';
 import AboutCompany from './components/AboutCompany';
 import SpaceVisualizer from './components/SpaceVisualizer';
 import VideoVisualizer from './components/VideoVisualizer';
-// VoiceConsultant는 라이브러리 문제 해결 후 나중에 추가하겠습니다.
-// import VoiceConsultant from './components/VoiceConsultant'; 
-import QuoteAI from './components/QuoteAI';
 import Services from './components/Services';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
@@ -20,8 +17,7 @@ function App() {
         <AboutCompany />
         <SpaceVisualizer />
         <VideoVisualizer />
-        {/* <VoiceConsultant /> - 에러 방지를 위해 잠시 주석 처리 */}
-        <QuoteAI />
+        {/* 에러 원인이 되는 VoiceConsultant와 QuoteAI는 잠시 제외합니다 */}
         <Services />
         <Testimonials />
       </main>
