0ae12bb487e5eaa1e74a65d9d49a635139fa7fd6
linkcleaning <han0829@gmail.com>
2026-01-29 22:38:51 +0900

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-// 폴더에 있는 컴포넌트들을 모두 불러옵니다.
 import Header from './components/Header';
 import Hero from './components/Hero';
 import AboutCompany from './components/AboutCompany';
 import SpaceVisualizer from './components/SpaceVisualizer';
 import VideoVisualizer from './components/VideoVisualizer';
-import VoiceConsultant from './components/VoiceConsultant';
+// VoiceConsultant는 라이브러리 문제 해결 후 나중에 추가하겠습니다.
+// import VoiceConsultant from './components/VoiceConsultant'; 
 import QuoteAI from './components/QuoteAI';
 import Services from './components/Services';
 import Testimonials from './components/Testimonials';
@@ -14,24 +14,17 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="min-h-screen bg-white">
-      {/* 1. 상단 메뉴 */}
       <Header />
-      
-      {/* 2. 메인 히어로 화면 (인테리어 배경) */}
       <Hero />
-      
-      {/* 3. 아래로 이어지는 섹션들 */}
       <main>
         <AboutCompany />
         <SpaceVisualizer />
         <VideoVisualizer />
-        <VoiceConsultant />
+        {/* <VoiceConsultant /> - 에러 방지를 위해 잠시 주석 처리 */}
         <QuoteAI />
         <Services />
         <Testimonials />
       </main>
-      
-      {/* 4. 하단 정보 */}
       <Footer />
     </div>
   );
