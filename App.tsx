56b98f007079fe3a44dfb5c365048d795d5d09e7
linkcleaning <han0829@gmail.com>
2026-01-29 23:59:37 +0900

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,8 +4,8 @@ import Hero from './components/Hero';
 import AboutCompany from './components/AboutCompany';
 import SpaceVisualizer from './components/SpaceVisualizer';
 import VideoVisualizer from './components/VideoVisualizer';
-import VoiceConsultant from './components/VoiceConsultant'; // 음성 상담 불러오기
-import Services from './components/Services';
+import VoiceConsultant from './components/VoiceConsultant';
+// import Services from './components/Services'; // 사용하지 않는 컴포넌트는 제외합니다.
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
 
@@ -18,8 +18,8 @@ function App() {
         <AboutCompany />
         <SpaceVisualizer />
         <VideoVisualizer />
-        <VoiceConsultant /> {/* 화면에 음성 상담 섹션 표시 */}
-        <Services />
+        <VoiceConsultant />
+        {/* <Services />  <-- 이 줄을 삭제하여 '청소 라인업' 섹션을 제거했습니다. */}
         <Testimonials />
       </main>
       <Footer />
