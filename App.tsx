cafd0d2c0bd44880b58837acf6f7f97363985682
linkcleaning <han0829@gmail.com>
2026-01-29 23:22:56 +0900

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import AboutCompany from './components/AboutCompany';
-// 에러를 일으키는 AI 관련 기능들은 잠시 제외합니다.
-// import SpaceVisualizer from './components/SpaceVisualizer';
-// import VideoVisualizer from './components/VideoVisualizer';
+import SpaceVisualizer from './components/SpaceVisualizer';
+import VideoVisualizer from './components/VideoVisualizer';
 import Services from './components/Services';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
@@ -16,7 +15,6 @@ function App() {
       <Hero />
       <main>
         <AboutCompany />
-        {/* 아래 기능들은 에러 해결 후 나중에 추가할 수 있습니다. */}
         <SpaceVisualizer />
         <VideoVisualizer />
         <Services />
